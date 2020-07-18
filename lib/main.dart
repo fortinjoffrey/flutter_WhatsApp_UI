854967dd9d93470201e6a46687debc9ef9b2d56f
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-18 14:40:04 +0200

    Implement TabBarView

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  Color whatsAppGreen = Color.fromRGBO(18, 140, 126, 1.0);
+  Color whatsAppGreenLight = Color.fromRGBO(37, 211, 102, 1.0);
+  TabController tabController;
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(vsync: this, length: 4);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello wolrd'),
+      appBar: AppBar(
+        title: Text(
+          'WhatsApp',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.search),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Icon(Icons.more_vert),
+          ),
+        ],
+        backgroundColor: whatsAppGreen,
+        bottom: TabBar(
+          controller: tabController,
+          indicatorColor: Colors.white,
+          tabs: [
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(child: Text('CHATS')),
+            Tab(child: Text('STATUS')),
+            Tab(child: Text('CALLS')),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          Icon(Icons.camera_alt),
+          Text("Chat Screen"),
+          Text("Status Screen"),
+          Text("Call Screen"),
+        ],
       ),
     );
   }
