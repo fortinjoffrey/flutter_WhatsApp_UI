b4ca93f9f01be0fbfad7bce4a04d7dbb9f2d9507
Joffrey Fortin <fortinjoffrey@gmail.com>
2020-07-18 14:28:16 +0200

    Upload initial flutter files

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello wolrd'),
+      ),
+    );
+  }
+}
